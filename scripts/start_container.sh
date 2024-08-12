99fcacc4caa380d2d0a7ff7da25ee3df92420825
Susmitha B <163541522+SusiGitLab@users.noreply.github.com>
2024-08-12 12:18:10 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull yasminjeelani/mypythonapp
+docker pull susidockerrepo/mypythonapp
 
 # Run the Docker image as a container
-docker run -d --name mypythoncontainer -p 5001:5000 yasminjeelani/mypythonapp
+docker run -d --name mypythoncontainer -p 5001:5000 susidockerrepo/mypythonapp
